605bc180533e9a76f712a1158c93524f2d0bc521
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-09-15 20:41:17 -0500

    Update env.py

--- prijateli_tree/migrations/env.py
+++ prijateli_tree/migrations/env.py
@@ -15,7 +15,7 @@ target_metadata = None
 
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode."""
+    """Run migrations in `offline` mode."""
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
@@ -29,7 +29,7 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode."""
+    """Run migrations in `online` mode."""
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",

6cff37344e40d2cdb73bca7bf59a928b0d4bdbf2
Federico Domínguez Molina <fd.molina@outlook.com>
2023-09-18 21:09:25 -0500

    Abstracted GameRound and PlayerRound

--- prijateli_tree/app/models.py
+++ prijateli_tree/app/models.py
@@ -1,8 +1,6 @@
 """Script that houses database models."""
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from sqlalchemy.sql import func
 
 Base = declarative_base()
 
@@ -25,9 +23,25 @@ class Player(Base):
     total_points = Column(Integer, nullable=False, default=0)
 
 
-class Game(Base):
-    __tablename__ = "games"
+class PlayerRound(Base):
+    __tablename__ = "player_rounds"
     game_id = Column(Integer, primary_key=True)
+    session_id = Column(Integer, ForeignKey("sessions.session_id"))
     player_name = Column(String(STR_LEN), nullable=False)
     player_id = Column(String(STR_LEN), nullable=False)
     player_points = Column(Integer, nullable=False, default=0)
+    # where the player is in the network
+    network_position = Column(Integer, nullable=False)
+    # random draw shown
+    random_draw = Column(Integer, nullable=False)
+
+
+class GameRound(Base):
+    __tablename__ = "game_rounds"
+    game_id = Column(Integer, primary_key=True)
+    # chosen bag
+    bag_id = Column(Integer, nullable=False)
+    # network condition
+    network_id = Column(Integer, nullable=False)
+    # Names condition
+    name_condition_id = Column(Integer, nullable=False)

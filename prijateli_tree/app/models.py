6c2597720349e39b81b913463ad76bb89aa9fcb8
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-09-18 20:50:26 -0500

    Update models.py

--- prijateli_tree/app/models.py
+++ prijateli_tree/app/models.py
@@ -1,8 +1,7 @@
 """Script that houses database models."""
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from sqlalchemy.sql import func
+
 
 Base = declarative_base()
 

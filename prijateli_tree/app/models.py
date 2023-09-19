a8d50f2deb8d925d50424f97c567f68a836ca0db
Federico Domínguez Molina <fd.molina@outlook.com>
2023-09-18 21:22:41 -0500

    Adding relationships and foreignkeys

--- prijateli_tree/app/models.py
+++ prijateli_tree/app/models.py
@@ -1,7 +1,6 @@
-"""Script that houses database models."""
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-
 
 Base = declarative_base()
 
@@ -12,8 +11,9 @@ class Session(Base):
     __tablename__ = "sessions"
     session_id = Column(Integer, primary_key=True)
     player_name = Column(String(STR_LEN), nullable=False)
-    player_id = Column(String(STR_LEN), nullable=False)
+    player_id = Column(Integer, ForeignKey("players.player_id"), nullable=False)
     player_language = Column(String(STR_LEN), nullable=False)
+    player = relationship("Player", back_populates="sessions")
 
 
 class Player(Base):
@@ -22,27 +22,25 @@ class Player(Base):
     player_name = Column(String(STR_LEN), nullable=False)
     player_language = Column(String(STR_LEN), nullable=False)
     total_points = Column(Integer, nullable=False, default=0)
+    sessions = relationship("Session", back_populates="player")
 
 
 class PlayerRound(Base):
     __tablename__ = "player_rounds"
     game_id = Column(Integer, primary_key=True)
-    session_id = Column(Integer, nullable=False)
+    session_id = Column(Integer, ForeignKey("sessions.session_id"), nullable=False)
     player_name = Column(String(STR_LEN), nullable=False)
-    player_id = Column(String(STR_LEN), nullable=False)
+    player_id = Column(Integer, ForeignKey("players.player_id"), nullable=False)
     player_points = Column(Integer, nullable=False, default=0)
-    # where the player is in the network
     network_position = Column(Integer, nullable=False)
-    # random draw shown
     random_draw = Column(Integer, nullable=False)
+    session = relationship("Session", back_populates="player_rounds")
+    player = relationship("Player", back_populates="player_rounds")
 
 
 class GameRound(Base):
     __tablename__ = "game_rounds"
     game_id = Column(Integer, primary_key=True)
-    # chosen bag
     bag_id = Column(Integer, nullable=False)
-    # network condition
     network_id = Column(Integer, nullable=False)
-    # Names condition
     name_condition_id = Column(Integer, nullable=False)

691f65b097d3ba28895781f820da0a21c0c1099a
Federico Domínguez Molina <fd.molina@outlook.com>
2023-09-18 21:25:36 -0500

    ISort for imports

--- prijateli_tree/app/models.py
+++ prijateli_tree/app/models.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 

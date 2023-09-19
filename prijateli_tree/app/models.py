01d30a8a7e6843e051882337344998c505b56817
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-09-19 10:34:03 -0500

    Update models.py

--- prijateli_tree/app/models.py
+++ prijateli_tree/app/models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Date, DateTime, ForeignKey, Identity, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func as sql_func
 
 
 Base = declarative_base()
@@ -8,18 +9,58 @@ Base = declarative_base()
 STR_LEN = 80
 
 
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, Identity(start=1, cycle=True), primary_key=True)
+    created_at = Column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=sql_func.now(),
+        unique=False,
+    )
+    first_name = Column(String, nullable=False, unique=False)
+    last_name = Column(String, nullable=False, unique=False)
+    email = Column(String, nullable=True, unique=True)
+    phone_number = Column(String, nullable=True, unique=True)
+    birth_date = Column(Date, nullable=True, unique=False)
+    language_id = Column(
+        Integer,
+        ForeignKey("languages.id", name="users_languages_id_fkey"),
+        nullable=False,
+    )
+
+
+class Language(Base):
+    __tablename__ = "languages"
+    id = Column(Integer, Identity(start=1, cycle=True), primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+    abbr = Column(String(2), nullable=False, unique=True)
+
+
 class Session(Base):
     __tablename__ = "sessions"
-    session_id = Column(Integer, primary_key=True)
+    id = Column(Integer, Identity(start=1, cycle=True), primary_key=True)
+    created_at = Column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=sql_func.now(),
+        unique=False,
+    )
     player_name = Column(String(STR_LEN), nullable=False)
-    player_id = Column(Integer, ForeignKey("players.player_id"), nullable=False)
+    player_id = Column(Integer, ForeignKey("players.id"), nullable=False)
     player_language = Column(String(STR_LEN), nullable=False)
     player = relationship("Player", back_populates="sessions")
 
 
 class Player(Base):
     __tablename__ = "players"
-    player_id = Column(Integer, primary_key=True)
+    id = Column(Integer, Identity(start=1, cycle=True), primary_key=True)
+    created_at = Column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=sql_func.now(),
+        unique=False,
+    )
     player_name = Column(String(STR_LEN), nullable=False)
     player_language = Column(String(STR_LEN), nullable=False)
     total_points = Column(Integer, nullable=False, default=0)
@@ -28,7 +69,7 @@ class Player(Base):
 
 class PlayerRound(Base):
     __tablename__ = "player_rounds"
-    game_id = Column(Integer, primary_key=True)
+    game_id = Column(Integer, Identity(start=1, cycle=True), primary_key=True)
     session_id = Column(Integer, ForeignKey("sessions.session_id"), nullable=False)
     player_name = Column(String(STR_LEN), nullable=False)
     player_id = Column(Integer, ForeignKey("players.player_id"), nullable=False)
@@ -41,7 +82,6 @@ class PlayerRound(Base):
 
 class GameRound(Base):
     __tablename__ = "game_rounds"
-    game_id = Column(Integer, primary_key=True)
-    bag_id = Column(Integer, nullable=False)
+    id = Column(Integer, Identity(start=1, cycle=True), primary_key=True)
     network_id = Column(Integer, nullable=False)
     name_condition_id = Column(Integer, nullable=False)

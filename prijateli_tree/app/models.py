f641918dceb99e7ee811fb851c43998c85142c78
Federico Domínguez Molina <fd.molina@outlook.com>
2023-09-18 20:04:22 -0500

    Adding player class

--- prijateli_tree/app/models.py
+++ prijateli_tree/app/models.py
@@ -5,3 +5,13 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
 Base = declarative_base()
+
+STR_LEN = 80
+
+
+class Player(Base):
+    __tablename__ = "players"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(STR_LEN), nullable=False)
+    language = Column(String(STR_LEN), nullable=False)
+    points_received = Column(Integer, nullable=False, default=0)

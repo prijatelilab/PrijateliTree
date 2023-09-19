1857f932d5906577a939512ac706cefa26e828ef
Federico Domínguez Molina <fd.molina@outlook.com>
2023-09-18 19:54:21 -0500

    Importing SQLAlchemy stuff for dev

--- prijateli_tree/app/models.py
+++ prijateli_tree/app/models.py
@@ -1,1 +1,7 @@
-"""Houses database models."""
+"""Script that houses database models."""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+Base = declarative_base()

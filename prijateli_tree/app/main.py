2711435c89556eb903179e8b33df029baaa2bcf9
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-09-22 17:18:42 -0500

    Update main.py

--- prijateli_tree/app/main.py
+++ prijateli_tree/app/main.py
@@ -1,19 +1,22 @@
+import os
+
 from fastapi import FastAPI
 from fastapi_sqlalchemy import DBSessionMiddleware
-from models.config import config
 
 from prijateli_tree.app.utils.constants import KEY_DATABASE_URI
 
 
 def create_application(config_name: str = "default"):
     fast_api_app = FastAPI()
-    fast_api_app.config = config[config_name]
 
-    fast_api_app.add_middleware(
-        DBSessionMiddleware, db_url=fast_api_app.config[KEY_DATABASE_URI]
-    )
+    fast_api_app.add_middleware(DBSessionMiddleware, db_url=os.getenv(KEY_DATABASE_URI))
 
     return fast_api_app
 
 
 app = create_application()
+
+
+@app.get("/")
+def funky():
+    return {"Hello": "World"}

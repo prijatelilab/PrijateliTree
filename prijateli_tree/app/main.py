3cdbe7244da469815ab513e5d2234fc4c3676c19
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-09-29 20:59:37 -0500

    Update main.py

--- prijateli_tree/app/main.py
+++ prijateli_tree/app/main.py
@@ -1,20 +1,15 @@
 import os
-import sys
 
-from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi_sqlalchemy import DBSessionMiddleware
 
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-load_dotenv(os.path.join(BASE_DIR, ".env"))
-sys.path.append(BASE_DIR)
+from prijateli_tree.app.utils.constants import KEY_DATABASE_URI
 
 
 def create_application():
     fast_api_app = FastAPI()
 
-    # fast_api_app.add_middleware(DBSessionMiddleware,
-    # db_url=os.getenv(KEY_DATABASE_URI))
+    fast_api_app.add_middleware(DBSessionMiddleware, db_url=os.getenv(KEY_DATABASE_URI))
 
     return fast_api_app
 

cc6742cd3976165e8e231ff537875541de421de7
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-10-02 00:24:01 -0500

    Update main.py

--- prijateli_tree/app/main.py
+++ prijateli_tree/app/main.py
@@ -7,7 +7,6 @@ from prijateli_tree.app.utils.constants import KEY_DATABASE_URI
 
 
 app = FastAPI()
-print(os.getenv(KEY_DATABASE_URI))
 
 app.add_middleware(DBSessionMiddleware, db_url=os.getenv(KEY_DATABASE_URI))
 

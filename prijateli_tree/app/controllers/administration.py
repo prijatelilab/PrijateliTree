10163bfdd8ec4dba5652e823aef8d78dfafa526f
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-10-06 11:10:42 -0500

    base structure

---
+++ prijateli_tree/app/controllers/administration.py
@@ -0,0 +1,2 @@
+def stuff():
+    print("things")
--- prijateli_tree/app/main.py
+++ prijateli_tree/app/main.py
@@ -3,6 +3,7 @@ import os
 from fastapi import FastAPI
 from fastapi_sqlalchemy import DBSessionMiddleware
 
+from prijateli_tree.app.controllers.administration import stuff
 from prijateli_tree.app.utils.constants import KEY_DATABASE_URI
 
 
@@ -14,3 +15,8 @@ app.add_middleware(DBSessionMiddleware, db_url=os.getenv(KEY_DATABASE_URI))
 @app.get("/")
 def funky():
     return {"Hello": "World"}
+
+
+@app.get("/administration")
+def admin_access():
+    stuff()

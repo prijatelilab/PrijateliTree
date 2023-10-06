1bfb21566a83c20072fc66a31ed574e7a58477ec
Michael Plunkett <5885605+michplunkett@users.noreply.github.com>
2023-10-06 10:52:55 -0500

    Add `session_types` and modify columns (#14)
    ## Describe Your Changes
    Added rows to `session_types`, updated columns on `session` related
    tables, and removed `pydantic` objects.
    
    ## Checklist Before Requesting a Review
    - [x] The code runs successfully.
    
    ```commandline
    (prijatelitree-py3.11) michaelp@MacBook-Air-5 PrijateliTree % docker-compose run web alembic --config=./prijateli_tree/migrations/alembic.ini upgrade head
    [+] Creating 1/0
     ✔ Container prijatelitree-postgres-1  Running                                                                                                                                                             0.0s 
    INFO  [alembic.runtime.migration] Context impl PostgresqlImpl.
    INFO  [alembic.runtime.migration] Will assume transactional DDL.
    INFO  [alembic.runtime.migration] Running upgrade 44f42b35d858 -> 47fe187bc716, empty message
    (prijatelitree-py3.11) michaelp@MacBook-Air-5 PrijateliTree % 
    ```

---
+++ prijateli_tree/migrations/versions/2023-10-05-2157_47fe187bc716_.py
@@ -0,0 +1,85 @@
+"""empty message
+
+Revision ID: 47fe187bc716
+Revises: 44f42b35d858
+Create Date: 2023-10-05 21:57:10.011419
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+
+revision = "47fe187bc716"
+down_revision = "44f42b35d858"
+
+
+def upgrade():
+    op.add_column(
+        "session_answers", sa.Column("session_player_id", sa.Integer(), nullable=False)
+    )
+    op.drop_constraint(
+        "session_answers_session_id_fkey", "session_answers", type_="foreignkey"
+    )
+    op.drop_constraint(
+        "session_answers_player_id_fkey", "session_answers", type_="foreignkey"
+    )
+    op.create_foreign_key(
+        "session_answers_session_players_fkey",
+        "session_answers",
+        "session_players",
+        ["session_player_id"],
+        ["id"],
+    )
+    op.drop_column("session_answers", "player_id")
+    op.drop_column("session_answers", "session_id")
+    op.add_column(
+        "session_players", sa.Column("position", sa.Integer(), nullable=False)
+    )
+
+    op.execute(
+        """
+            INSERT INTO session_types
+            (network, bag)
+            VALUES
+            ('integrated', 'RRRRBB'),
+            ('segregated', 'RRRRBB'),
+            ('self-selected', 'RRRRBB')
+        """
+    )
+
+
+def downgrade():
+    op.drop_column("session_players", "position")
+    op.add_column(
+        "session_answers",
+        sa.Column("session_id", sa.INTEGER(), autoincrement=False, nullable=False),
+    )
+    op.add_column(
+        "session_answers",
+        sa.Column("player_id", sa.INTEGER(), autoincrement=False, nullable=False),
+    )
+    op.drop_constraint(
+        "session_answers_session_players_fkey", "session_answers", type_="foreignkey"
+    )
+    op.create_foreign_key(
+        "session_answers_player_id_fkey",
+        "session_answers",
+        "users",
+        ["player_id"],
+        ["id"],
+    )
+    op.create_foreign_key(
+        "session_answers_session_id_fkey",
+        "session_answers",
+        "sessions",
+        ["session_id"],
+        ["id"],
+    )
+    op.drop_column("session_answers", "session_player_id")
+
+    op.execute(
+        """
+            DELETE FROM session_types
+            WHERE bag = 'RRRRBB';
+        """
+    )

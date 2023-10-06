88cab21d29af4ef47ba7c9e38a364f81d02bfbfb
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-10-06 11:42:54 -0500

    Update 2023-10-06-1624_e5b498dd07a6_.py

--- prijateli_tree/migrations/versions/2023-10-06-1624_e5b498dd07a6_.py
+++ prijateli_tree/migrations/versions/2023-10-06-1624_e5b498dd07a6_.py
@@ -6,18 +6,16 @@ Create Date: 2023-10-06 16:24:21.643648
 
 """
 from alembic import op
-import sqlalchemy as sa
 
 
-
-revision = 'e5b498dd07a6'
-down_revision = '47fe187bc716'
+revision = "e5b498dd07a6"
+down_revision = "47fe187bc716"
 
 
 def upgrade():
     op.execute(
         """
-            INSERT INTO session_types
+            INSERT INTO languages
             (name, abbr)
             VALUES
             ('English', 'EN'),

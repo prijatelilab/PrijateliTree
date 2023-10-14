66b6ace32ab2909a2ca5909b66793f574f065de5
Federico Domínguez Molina <fd.molina@outlook.com>
2023-10-14 18:45:01 -0500

    Adding datetime to create game in db

--- prijateli_tree/app/utils/database.py
+++ prijateli_tree/app/utils/database.py
@@ -1,5 +1,6 @@
 """Utility class and functions to store and retrieve data from the database."""
 import psycopg2
+from datetime import datetime
 
 # Local imports
 from keys import DB_CREDS
@@ -23,10 +24,16 @@ class DatabaseHandler:
         states and data.
         """
 
+        now = datetime.now()
+        created_at = now.strftime("%Y-%m-%d %H:%M:%S")
+
         cursor = self.connection.cursor()
+        created_at = datetime.now()
         QUERY = f"""
-            INSERT INTO games (id, created_by, game_type_id, rounds, practice)
-            VALUES ({game_id}, {user_id}, {game_type}, {rounds}, {practice});
+            INSERT INTO games (id, created_at, created_by, game_type_id, rounds,
+            practice)
+            VALUES ({game_id}, '{created_at}', {user_id}, {game_type}, 
+            {rounds}, {practice});
         """
 
         cursor.execute(QUERY)
--- prijateli_tree/app/views/games.py
+++ prijateli_tree/app/views/games.py
@@ -2,14 +2,28 @@
 Contains baseline structure for all three game types.
 """
 from prijateli_tree.app.models.database import Game
-
-# We may need to create a database class
-# to handle all database interactions
+from prijateli_tree.app.utils.constants import (
+    NETWORK_TYPE_INTEGRATED,
+    NETWORK_TYPE_SEGREGATED,
+    NETWORK_TYPE_SELF_SELECTED,
+)
+from prijateli_tree.app.utils.database import DatabaseHandler
 
 
 def integrated_game(game: Game, player_id: int):
     # Fetch a game and player from the database with their
     # respective neighbors
+
+    database = DatabaseHandler()
+
+    # Create integrated network game
+    game = Game(NETWORK_TYPE_INTEGRATED)
+    game_id = 1
+    user_id = 1
+    game_type_id = 1
+
+    # Create game in database
+    database.create_game(game)
 
     # game = database.fetch_game_by_id(game_id)
     # player = database.fetch_player_by_id(player_id)
@@ -66,148 +80,3 @@ def segregated_game(game: Game, player_id: int):
 
 def self_selected_game(game: Game, player_id: int):
     return {"game_id": game.id, "player_id": player_id}
-
-
-# class Game:
-#     def __init__(self, max_rounds, network_type, correct_points):
-#         self.players = []
-#         self.max_rounds = max_rounds
-#         self.current_round = 0
-#         self.network_type = network_type
-#         self.correct_points = correct_points
-#         # Bag compositions
-#         self.bags = {
-#             "red": ["red", "red", "red", "red", "blue", "blue"],
-#             "blue": ["blue", "blue", "blue", "blue", "red", "red"],
-#         }
-
-#         self.network_methods = {
-#             "integrated": self._create_integrated_network,
-#             "segregated": self._create_segregated_network,
-#             "self_select": self._create_self_select_network,
-#         }
-
-#     def assign_network(self, network_type):
-#         """
-#         Assigns a network structure to the game
-#         """
-#         if network_type not in self.network_methods:
-#             raise ValueError(f"Unknown network type: {network_type}")
-#         self.network = self.network_methods[network_type]()
-
-#     def _create_integrated_network(self):
-#         """
-#         Creates an integrated network, where players observe a player from their
-#         own nationality and from another
-#         """
-#         shuffle(self.players)
-#         network = {}
-
-#         # TO DO: Add logic to assign neighbors
-
-#         return network
-
-#     def _create_segregated_network(self):
-#         shuffle(self.players)
-#         network = {}
-
-#         # TO DO: Add logic to assign neighbors
-
-#         return network
-
-#     def _create_self_select_network(self):
-#         """
-#         Creates a self selected network where players choose their neighbors.
-#         """
-#         network = {}
-#         # TO DO: Add logic to assign neighbors
-#         return network
-
-#     def add_player(self, player):
-#         self.players.append(player)
-
-#     def setup_game(self):
-#         self.network_structure = self.decide_structure()
-#         self.bag = self.draw_bag()
-
-#     def draw_bag(self):
-#         """
-#         Randomly selects either a red or blue bag.
-#         """
-#         return random.choice(["red", "blue"])
-
-#     def distribute_balls(self):
-#         for player in self.players:
-#             ball = self.draw_ball_from_bag()
-#             player.observe(ball)  # Hypothetical method
-
-#     def draw_ball_from_bag(self):
-#         # Randomly draw a ball based on the bag's composition
-#         ball = random.choice(self.bags[self.bag])
-#         # Remove the drawn ball from the bag
-#         self.bags[self.bag].remove(ball)
-#         return ball
-
-#     def play_round(self):
-#         if self.current_round == 0:
-#             # First round logic: Players simply observe and make guesses
-#             for player in self.players:
-#                 ball = self.draw_ball_from_bag()
-#                 player.observe(ball)
-#                 guess = player.make_guess()
-#                 self.guesses[player.player_id] = guess
-#         else:
-#             # Players observe their ball and previous guesses of neighbors
-#             for player in self.players:
-#                 ball = self.draw_ball_from_bag()
-#                 player.observe(ball)
-#                 neighbor_guesses = self.get_neighbor_guesses(player)
-#                 player.observe_others_guesses(neighbor_guesses)
-#                 updated_guess = (
-#                     player.update_guess()
-#                 )  # Assuming players might change their guess
-#                 self.guesses[player.player_id] = updated_guess
-
-#         self.current_round += 1
-
-#     def get_neighbor_guesses(self, player):
-#         """
-#         Returns a dictionary of player_id: guess pairs for the player's neighbors.
-#         """
-#         neighbor_guesses = {}
-#         for neighbor in self.network_structure[player.player_id]:
-#             neighbor_guesses[neighbor] = self.guesses[neighbor]
-#         return neighbor_guesses
-
-#     def calculate_points(self, player):
-#         """
-#         Calculates the points for a player based on their
-#         guess and the drawn bag.
-#         """
-#         player_guess = player.guess
-#         if player_guess == self.bag:
-#             print(f"Correct guess!, you will get {self.correct_points}")
-#             return self.correct_points
-#         else:
-#             print(
-#                 f"Better luck next time!, you would have gotten {self.correct_points} "
-#                 "points"
-#             )
-#             return 0
-
-#     def end_game(self):
-#         """
-#         Calculates the points for each player and adds them to their score.
-#         """
-#         for player in self.players:
-#             points_won = self.calculate_points(player)
-#             player.add_points(points_won)
-
-#     def play_game(self):
-#         """
-#         Executes the game pipeline.
-#         """
-#         self.setup_game()
-#         while self.current_round < self.max_rounds:
-#             self.play_round()
-#         self.end_game()

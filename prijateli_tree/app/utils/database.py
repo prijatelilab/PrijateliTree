2f51a27783cfe0de2622e3e9ef3ef902d19f4292
Federico Domínguez Molina <fd.molina@outlook.com>
2023-10-14 00:03:04 -0500

    Adding database handler class

--- prijateli_tree/app/utils/database.py
+++ prijateli_tree/app/utils/database.py
@@ -1,9 +1,30 @@
 """Utility class and functions to store and retrieve data from the database."""
+import psycopg2
+
+# Local imports
+from keys import DB_CREDS
 
 
 class DatabaseHandler:
-    def __init__(self, session):
-        self.session = session
+    """Class to handle database connections."""
 
-    def create_game(self):
-        pass
+    def __init__(self):
+        self.connection = psycopg2.connect(
+            user=DB_CREDS["username"],
+            password=DB_CREDS["password"],
+            database=DB_CREDS["database"],
+            host=DB_CREDS["host"],
+            port=DB_CREDS["port"],
+        )
+
+    def fetch_game_by_id(self, game_id):
+        """
+        Function used to create and handle game
+        states and data.
+        """
+
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM games WHERE id = %s", (game_id,))
+        game = cursor.fetchone()
+        cursor.close()
+        return game

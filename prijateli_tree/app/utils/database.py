b33aadcca76115cb1bfcf10c8c453f7bda43954e
Federico Domínguez Molina <fd.molina@outlook.com>
2023-10-13 23:47:47 -0500

    Adding handler script for db

---
+++ prijateli_tree/app/utils/database.py
@@ -0,0 +1,9 @@
+"""Utility class and functions to store and retrieve data from the database."""
+
+
+class DatabaseHandler:
+    def __init__(self, session):
+        self.session = session
+
+    def create_game(self):
+        pass

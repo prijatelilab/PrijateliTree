aae6e2f745e8d9725f5d1477df79d09e82107d7b
Federico Domínguez Molina <fd.molina@outlook.com>
2023-10-14 20:38:41 -0500

    Removing unused comments

--- prijateli_tree/app/views/games.py
+++ prijateli_tree/app/views/games.py
@@ -52,52 +52,13 @@ def integrated_game(game: Game, game_id: int, player_id: int):
     game = database.fetch_game_by_id(game_id)
     player = database.fetch_player_by_id(player_id)
 
-    # Create integrated network game
-    # game = database.fetch_game_by_id(game_id)
-    # player = database.fetch_player_by_id(player_id)
-    # neighbors = database.fetch_neighbors_for_player_in_game(game_id, player_id)
+    # Check if the game or player doesn't exist
+    if not game or not player:
+        raise ValueError("Game or player not found!")
 
-    #   if game.current_round == 0:
-    #       This updates the Neighbor Relationships table
-    #       network = create_integrated_network(game_id, game.players)
-    #       bag = game.draw_bag()
-    #       distribute_balls_to_players(game_id, bag)
-
-    #       # Fetch the ball for the player for this round
-    #       player_ball = database.fetch_ball_for_player_in_round(game_id, player_id,
-    #                                                             game.current_round)
-
-    #       # Players make their initial guess based on their ball alone
-    #       player_initial_guess = player.make_initial_guess(player_ball)
-    #       # Update player's guess in PlayerRounds table
-    #       database.update_player_guess_for_round(game_id, player_id, game.current_round,
-    #                                                player_initial_guess)
-
-    # else:
-
-    #     Fetch the ball for the player for this round
-    #     player_ball = database.fetch_ball_for_player_in_round(game_id, player_id, game.current_round)
-
-    #     # Players make their guess based on their ball and the guesses of their neighbors  # noqa: E501
-    #     # Fetch neighbors' previous round guesses
-    #     neighbor_guesses = database.fetch_guesses_for_neighbors_in_round(game_id,
-    #                                                                      neighbors,
-    #                                                                      game.current_round - 1)
-
-    #     player_updated_guess = player.update_guess(player_ball, neighbor_guesses)
-
-    #     # Update player's guess in PlayerRounds table
-    #     database.update_player_guess_for_round(game_id, player_id, game.current_round,
-    #                                            player_updated_guess)
-
-    # # If the game is ending, calculate scores
-    # if game.is_ending():
-    #     points_won = game.calculate_points(player)
-    #     database.update_score(player_id, points_won)
-
-    # # Update any other changes to the game or player in the database
-    # database.update_game(game)
-    # database.update_player(player)
+    # Check if the game is of type 'integrated'
+    if game.session_type.network != NETWORK_TYPE_INTEGRATED:
+        raise ValueError("This is not an integrated game!")
 
     return {"game_id": game.id, "player_id": player_id}
 

c4a1937f44f2c77a6adb8a5cf7511975bf5b2d7b
Federico Domínguez Molina <fd.molina@outlook.com>
2023-10-17 23:36:08 -0500

    Adding more schemas

--- prijateli_tree/app/schemas.py
+++ prijateli_tree/app/schemas.py
@@ -14,3 +14,53 @@ class User(BaseModel):
     birth_date: datetime
     role: str
     language_id: int
+    language: Language
+    denirs: Denirs
+
+
+class Language(BaseModel):
+    id: int
+    created_at: datetime
+    name: str
+    abbr: str
+    users: User
+
+
+class Denirs(BaseModel):
+    id: int
+    created_at: datetime
+    created_by_game_id: int
+    created_by_user_id: int
+    external_id: str
+    user: User
+
+
+class GameType(BaseModel):
+    id: int
+    created_at: datetime
+    network: str
+    bag: str
+    games: Game
+
+
+class Game(BaseModel):
+    id: int
+    created_at: datetime
+    created_by: int
+    game_type_id: int
+    rounds: int
+    practice: bool
+    game_type: GameType
+    players: Player
+
+
+class Player(BaseModel):
+    id: int
+    created_at: datetime
+    created_by: int
+    game_id: int
+    user_id: int
+    position: int
+    name_hidden: bool
+    session: Game
+    answers: GameAnswer

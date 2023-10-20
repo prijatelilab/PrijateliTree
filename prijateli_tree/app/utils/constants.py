ec4cecf5f7c201ec36039941398045bf7c42ab52
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-10-20 15:37:15 -0500

    Update constants.py

--- prijateli_tree/app/utils/constants.py
+++ prijateli_tree/app/utils/constants.py
@@ -10,3 +10,11 @@ KEY_ENV_TESTING = "testing"
 NETWORK_TYPE_INTEGRATED = "integrated"
 NETWORK_TYPE_SEGREGATED = "segregated"
 NETWORK_TYPE_SELF_SELECTED = "self-selected"
+
+# Validating Regex Constants
+REGEX_EMAIL = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b"
+# Source link: https://stackoverflow.com/a/51829465
+REGEX_PHONE_NUMBER = (
+    r"^(?:\d{8}(?:\d{2}(?:\d{2})?)?|\(\+?\d{2,3}\)\s?(?:\d{4}[\s*.-]?\d{4}"
+    r"|\d{3}[\s*.-]?\d{3}|\d{2}([\s*.-]?)\d{2}\1\d{2}(?:\1\d{2})?))$"
+)

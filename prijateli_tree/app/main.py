e5bc8048c02ca65e85e850caa4bfd1440fa8586a
Michael Plunkett <5885605+michplunkett@users.noreply.github.com>
2023-10-22 16:42:25 -0500

    Add ability to set language (#18)
    ## Describe Your Changes
    Migrated the `yml` files to `json` and added an endpoint to set the
    default language.
    
    ## Non-Obvious Technical Information
    The json files make use of a lot of unicode characters, but provided we
    are using `utf-8` encoding, we should be 💯 .
    
    ## Checklist Before Requesting a Review
    - [x] The code runs successfully.
    
    ```commandline
    (prijatelitree-py3.11) michaelp@MacBook-Air-7 PrijateliTree % curl -i -X POST -H 'Accept-Language: en' http://localhost:8000/language
    HTTP/1.1 200 OK
    date: Sun, 22 Oct 2023 18:46:14 GMT
    server: uvicorn
    content-length: 28
    content-type: application/json
    
    {"message":"It done worked"}%                                                                                                                                                                                   (prijatelitree-py3.11) michaelp@MacBook-Air-7 PrijateliTree % curl -i -X POST -H 'Accept-Language: tr' http://localhost:8000/language
    HTTP/1.1 200 OK
    date: Sun, 22 Oct 2023 18:47:00 GMT
    server: uvicorn
    content-length: 28
    content-type: application/json
    
    {"message":"It done worked"}%
    ```
    
    ```commandline
    2023-10-22 13:46:14 prijatelitree-web-1       | 2023-10-22 18:46:14,395 INFO sqlalchemy.engine.Engine COMMIT
    2023-10-22 13:46:14 prijatelitree-web-1       | INFO:     Started server process [23]
    2023-10-22 13:46:14 prijatelitree-web-1       | INFO:     Waiting for application startup.
    2023-10-22 13:46:14 prijatelitree-web-1       | INFO:     Application startup complete.
    2023-10-22 13:46:14 prijatelitree-web-1       | !!!!!!! en
    2023-10-22 13:46:14 prijatelitree-web-1       | INFO:     192.168.65.1:61869 - "POST /language HTTP/1.1" 200 OK
    2023-10-22 13:47:00 prijatelitree-web-1       | !!!!!!! tr
    2023-10-22 13:47:00 prijatelitree-web-1       | INFO:     192.168.65.1:61870 - "POST /language HTTP/1.1" 200 OK
    ```

--- prijateli_tree/app/main.py
+++ prijateli_tree/app/main.py
@@ -1,11 +1,26 @@
+import glob
+import json
+import os
 from http import HTTPStatus
+from typing import Annotated, List
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Header, HTTPException, Response
+from fastapi.responses import JSONResponse
+from fastapi_localization import TranslateJsonResponse
 
+from prijateli_tree.app.config import config
 from prijateli_tree.app.database import Base, Game, SessionLocal, engine
+from prijateli_tree.app.schemas import LanguageTranslatableSchema
 from prijateli_tree.app.utils.constants import (
+    FILE_MODE_READ,
+    KEY_ENV,
+    LANGUAGE_ALBANIAN,
+    LANGUAGE_ENGLISH,
+    LANGUAGE_MACEDONIAN,
+    LANGUAGE_TURKISH,
     NETWORK_TYPE_INTEGRATED,
     NETWORK_TYPE_SEGREGATED,
+    STANDARD_ENCODING,
 )
 from prijateli_tree.app.views.administration import stuff
 from prijateli_tree.app.views.games import (
@@ -21,14 +36,43 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+config = config[os.getenv(KEY_ENV)]
 
-# Dependency
+active_language = LANGUAGE_ENGLISH
+
+languages = {}
+for lang in glob.glob("languages/*.json"):
+    lang_code = lang.split("\\")[1].split(".")[0]
+
+    with open(lang, FILE_MODE_READ, encoding=STANDARD_ENCODING) as file:
+        languages[lang_code] = json.load(file)
+
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+
+@app.post(
+    "/language",
+    response_class=TranslateJsonResponse,
+    response_model=List[LanguageTranslatableSchema],
+)
+def set_language(accept_language: Annotated[str | None, Header()] = None) -> Response:
+    if accept_language in [
+        LANGUAGE_ENGLISH,
+        LANGUAGE_TURKISH,
+        LANGUAGE_MACEDONIAN,
+        LANGUAGE_ALBANIAN,
+    ]:
+        config.LANGUAGE = accept_language
+    else:
+        config.LANGUAGE = LANGUAGE_ENGLISH
+
+    return JSONResponse(content={"message": "It done worked"})
 
 
 @app.get("/")

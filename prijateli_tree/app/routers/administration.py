24a5ed1cfad89e2fa5634fc329ee3b8f61bf764a
Michael Plunkett <5885605+michplunkett@users.noreply.github.com>
2023-10-26 22:10:28 -0500

    Split router into admin and games (#22)
    ## Describe Your Changes
    Split the routers into administration and games.
    
    ## Checklist Before Requesting a Review
    - [x] The code runs successfully.
    
    ```commandline
    2023-10-26 22:08:01 prijatelitree-web-1       | INFO:     Started server process [56]
    2023-10-26 22:08:01 prijatelitree-web-1       | INFO:     Waiting for application startup.
    2023-10-26 22:08:01 prijatelitree-web-1       | INFO:     Application startup complete.
    2023-10-26 22:08:42 prijatelitree-web-1       | INFO:     192.168.65.1:62229 - "GET / HTTP/1.1" 200 OK
    ```

---
+++ prijateli_tree/app/routers/administration.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+
+from prijateli_tree.app.database import SessionLocal
+
+
+router = APIRouter()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.get("/")
+def admin_page():
+    return {"message": "Admin getting schwifty"}

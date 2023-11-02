cc5bf6d009107a668f28441b4e95b029a9a7f95a
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-11-02 14:22:21 -0500

    Update 2023-11-02-1917_21c9591d0d5d_.py

--- prijateli_tree/migrations/versions/2023-11-02-1917_21c9591d0d5d_.py
+++ prijateli_tree/migrations/versions/2023-11-02-1917_21c9591d0d5d_.py
@@ -5,16 +5,17 @@ Revises: 5489fb45e45e
 Create Date: 2023-11-02 19:17:52.387308
 
 """
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 
-revision = '21c9591d0d5d'
-down_revision = '5489fb45e45e'
+
+revision = "21c9591d0d5d"
+down_revision = "5489fb45e45e"
 
 
 def upgrade():
-    op.add_column('game_players', sa.Column('ready', sa.Boolean(), nullable=False))
+    op.add_column("game_players", sa.Column("ready", sa.Boolean(), nullable=False))
 
 
 def downgrade():
-    op.drop_column('game_players', 'ready')
+    op.drop_column("game_players", "ready")

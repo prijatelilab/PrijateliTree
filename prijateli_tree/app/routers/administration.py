a922ee4bcca107f8e92903f9a1aa19ccfc796181
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-11-04 00:13:22 -0500

    Update administration.py

--- prijateli_tree/app/routers/administration.py
+++ prijateli_tree/app/routers/administration.py
@@ -1,5 +1,4 @@
 import os
-from datetime import datetime
 from http import HTTPStatus
 from pathlib import Path
 from typing import Annotated
@@ -9,7 +8,6 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi_login import LoginManager
 from fastapi_login.exceptions import InvalidCredentialsException
-from sqlalchemy import or_
 from sqlalchemy.orm import Session
 
 from prijateli_tree.app.database import SessionLocal, User
@@ -34,17 +32,16 @@ def get_db():
 
 router = APIRouter()
 
-login_manager = LoginManager(os.getenv(KEY_LOGIN_SECRET), "/login")
+login_manager = LoginManager(
+    os.getenv(KEY_LOGIN_SECRET),
+    "/login",
+    use_cookie=True,
+)
 
 
-@login_manager.user_loader()
-def query_user(user_id: int, db: Session = Depends(get_db)):
-    return (
-        db.query(User)
-        .filter_by(id=user_id)
-        .filter(or_(User.role == ROLE_ADMIN, User.role == ROLE_SUPER_ADMIN))
-        .one_or_none()
-    )
+@login_manager.user_loader(db_session=SessionLocal())
+def query_user(user_id: int, db_session: Session):
+    return db_session.query(User).filter_by(id=user_id).one_or_none()
 
 
 @router.get("/", response_class=HTMLResponse)
@@ -64,28 +61,39 @@ def admin_login(request: Request):
 
 @router.post("/login")
 def confirm_login(
+    first_name: Annotated[str, Form()],
+    last_name: Annotated[str, Form()],
     email: Annotated[str, Form()],
-    birthdate_str: Annotated[str, Form()],
     db: Session = Depends(get_db),
 ):
-    birthdate = datetime.strptime(birthdate_str, "%m/%d/%Y")
     user = (
         db.query(User)
-        .filter_by(email=email.lower(), birthdate=birthdate)
-        .filter(or_(User.role == ROLE_ADMIN, User.role == ROLE_SUPER_ADMIN))
+        .filter_by(
+            email=email.lower(), first_name=first_name, last_name=last_name
+        )
+        .filter((User.role == ROLE_ADMIN) | (User.role == ROLE_SUPER_ADMIN))
         .one_or_none()
     )
     if user is None:
         raise InvalidCredentialsException
 
-    token = login_manager.create_access_token(data={"sub": email})
-    response = RedirectResponse(url="/protected", status_code=HTTPStatus.FOUND)
+    token = login_manager.create_access_token(data={"sub": user.id})
+    response = RedirectResponse(url="dashboard", status_code=HTTPStatus.FOUND)
     login_manager.set_cookie(response, token)
     return response
 
 
-@router.get("/dashboard")
-def dashboard(request: Request, user=Depends(login_manager)):
+@router.get("/logout", response_class=HTMLResponse)
+def logout(request: Request, user=Depends(login_manager)):
+    resp = RedirectResponse(url="admin_login", status_code=HTTPStatus.FOUND)
+    login_manager.set_cookie(resp, "")
+    return resp
+
+
+@router.get("/dashboard", response_class=HTMLResponse)
+def dashboard(
+    request: Request, user=Depends(login_manager), db: Session = Depends(get_db)
+):
     return templates.TemplateResponse(
         "admin_dashboard.html", {"request": request}
     )

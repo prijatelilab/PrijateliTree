ab146012e9921bfaafba1f222d1e86fb488fd266
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-11-09 21:29:34 -0600

    Update integrated_game_data.sql

--- misc/integrated_game_data.sql
+++ misc/integrated_game_data.sql
@@ -1,17 +1,17 @@
 /* Create users */
 INSERT INTO users
-    (created_by, first_name, last_name, email, birth_date, qualtrics_id, role, grade_level, language_id, high_school_id, uuid)
+    (created_by, first_name, last_name, email, birth_date, qualtrics_id, role, grade_level, language_id, high_school_id)
 VALUES
-    (NULL, 'admin', 'name', 'a@email.com', '01/01/2000'::date, 1, 'super-admin', 1, 1, 1, gen_random_uuid ()),
-    (1, 'macedonian', 'name', 'b@email.com', '01/01/2001'::date, 1, 'student', 2, 1, 1, gen_random_uuid ()),
-    (1, 'macedonian', 'name', 'c@email.com', '01/01/2002'::date, 1,  'student', 2, 2, 1, gen_random_uuid ()),
-    (1, 'macedonian', 'name', 'd@email.com', '01/01/2003'::date, 1, 'student', 2, 3, 2, gen_random_uuid ()),
-    (1, 'albanian', 'name', 'e@email.com', '01/01/2004'::date, 1, 'student', 3, 1, 2, gen_random_uuid ()),
-    (1, 'albanian', 'name', 'f@email.com', '01/01/2005'::date, 1, 'student', 3, 2, 2, gen_random_uuid ()),
-    (1, 'albanian', 'name', 'g@email.com', '01/01/2006'::date, 1, 'student', 3, 3, 3, gen_random_uuid ()),
-    (1, 'turkish', 'name', 'h@email.com', '01/01/2007'::date, 1, 'student', 4, 1, 3, gen_random_uuid ()),
-    (1, 'turkish', 'name', 'i@email.com', '01/01/2008'::date, 1, 'student', 4, 2, 3, gen_random_uuid ()),
-    (1, 'turkish', 'name', 'j@email.com', '01/01/2009'::date, 1, 'student', 4, 3, 3, gen_random_uuid ());
+    (NULL, 'admin', 'name', 'a@email.com', '01/01/2000'::date, 1, 'super-admin', 1, 1, 1),
+    (1, 'macedonian', 'name', 'b@email.com', '01/01/2001'::date, 1, 'student', 2, 1, 1),
+    (1, 'macedonian', 'name', 'c@email.com', '01/01/2002'::date, 1,  'student', 2, 2, 1),
+    (1, 'macedonian', 'name', 'd@email.com', '01/01/2003'::date, 1, 'student', 2, 3, 2),
+    (1, 'albanian', 'name', 'e@email.com', '01/01/2004'::date, 1, 'student', 3, 1, 2),
+    (1, 'albanian', 'name', 'f@email.com', '01/01/2005'::date, 1, 'student', 3, 2, 2),
+    (1, 'albanian', 'name', 'g@email.com', '01/01/2006'::date, 1, 'student', 3, 3, 3),
+    (1, 'turkish', 'name', 'h@email.com', '01/01/2007'::date, 1, 'student', 4, 1, 3),
+    (1, 'turkish', 'name', 'i@email.com', '01/01/2008'::date, 1, 'student', 4, 2, 3),
+    (1, 'turkish', 'name', 'j@email.com', '01/01/2009'::date, 1, 'student', 4, 3, 3);
 
 
 /* Create game */

be0c741d42d45c019575026f95c2267a372c2b42
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-11-14 14:50:08 -0600

    Update integrated_game_data.sql

--- misc/integrated_game_data.sql
+++ misc/integrated_game_data.sql
@@ -3,15 +3,15 @@ INSERT INTO users
     (first_name, last_name, email, birth_date, role, grade_level, language_id, high_school_id)
 VALUES
     ('admin', 'name', 'a@email.com', '01/01/2000'::date, 'super-admin', 1, 1, 1),
-    ('macedonian', 'name', 'b@email.com', '01/01/2001'::date, 'student', 2, 1, 1),
-    ('macedonian', 'name', 'c@email.com', '01/01/2002'::date,  'student', 2, 2, 1),
-    ('macedonian', 'name', 'd@email.com', '01/01/2003'::date, 'student', 2, 3, 2),
-    ('albanian', 'name', 'e@email.com', '01/01/2004'::date, 'student', 3, 1, 2),
-    ('albanian', 'name', 'f@email.com', '01/01/2005'::date, 'student', 3, 2, 2),
-    ('albanian', 'name', 'g@email.com', '01/01/2006'::date, 'student', 3, 3, 3),
-    ('turkish', 'name', 'h@email.com', '01/01/2007'::date, 'student', 4, 1, 3),
-    ('turkish', 'name', 'i@email.com', '01/01/2008'::date, 'student', 4, 2, 3),
-    ('turkish', 'name', 'j@email.com', '01/01/2009'::date, 'student', 4, 3, 3);
+    ('macedonian 1', 'name', 'b@email.com', '01/01/2001'::date, 'student', 2, 1, 1),
+    ('macedonian 2', 'name', 'c@email.com', '01/01/2002'::date,  'student', 2, 2, 1),
+    ('macedonian 3', 'name', 'd@email.com', '01/01/2003'::date, 'student', 2, 3, 2),
+    ('albanian 1', 'name', 'e@email.com', '01/01/2004'::date, 'student', 3, 1, 2),
+    ('albanian 2', 'name', 'f@email.com', '01/01/2005'::date, 'student', 3, 2, 2),
+    ('albanian 3', 'name', 'g@email.com', '01/01/2006'::date, 'student', 3, 3, 3),
+    ('turkish 1', 'name', 'h@email.com', '01/01/2007'::date, 'student', 4, 1, 3),
+    ('turkish 2', 'name', 'i@email.com', '01/01/2008'::date, 'student', 4, 2, 3),
+    ('turkish 3', 'name', 'j@email.com', '01/01/2009'::date, 'student', 4, 3, 3);
 
 
 /* Create game */

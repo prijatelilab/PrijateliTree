179551021bdec5298bb52d9af600704950d0a9eb
Michael Plunkett <5885605+michplunkett@users.noreply.github.com>
2023-11-15 01:27:04 -0600

    Add Create Game functionality (#38)
    ## Describe Your Changes
    Added the create game endpoint and filled out its respective form.
    
    ## Checklist Before Requesting a Review
    - [x] The code runs successfully.
    
    ```commandline
    2023-11-15 01:24:19 prijatelitree-web-1       | INFO:     Application startup complete.
    2023-11-15 01:24:19 prijatelitree-web-1       | INFO:     192.168.65.1:59556 - "GET /admin/dashboard_create_game HTTP/1.1" 404 Not Found
    2023-11-15 01:24:26 prijatelitree-web-1       | INFO:     192.168.65.1:59557 - "GET /admin/ HTTP/1.1" 302 Found
    2023-11-15 01:24:26 prijatelitree-web-1       | INFO:     192.168.65.1:59557 - "GET /admin/dashboard HTTP/1.1" 200 OK
    2023-11-15 01:24:27 prijatelitree-web-1       | INFO:     192.168.65.1:59557 - "GET /admin/game HTTP/1.1" 200 OK
    2023-11-15 01:24:28 prijatelitree-web-1       | INFO:     192.168.65.1:59557 - "POST /admin/game HTTP/1.1" 302 Found
    2023-11-15 01:24:28 prijatelitree-web-1       | INFO:     192.168.65.1:59557 - "GET /admin/game HTTP/1.1" 200 OK
    2023-11-15 01:24:29 prijatelitree-web-1       | INFO:     192.168.65.1:59557 - "POST /admin/game HTTP/1.1" 302 Found
    2023-11-15 01:24:29 prijatelitree-web-1       | INFO:     192.168.65.1:59557 - "GET /admin/game HTTP/1.1" 200 OK
    2023-11-15 01:24:29 prijatelitree-web-1       | INFO:     192.168.65.1:59557 - "GET /css/custom.css HTTP/1.1" 304 Not Modified
    2023-11-15 01:24:30 prijatelitree-web-1       | INFO:     192.168.65.1:59557 - "POST /admin/game HTTP/1.1" 302 Found
    2023-11-15 01:24:30 prijatelitree-web-1       | INFO:     192.168.65.1:59557 - "GET /admin/game HTTP/1.1" 200 OK
    2023-11-15 01:24:30 prijatelitree-web-1       | INFO:     192.168.65.1:59557 - "POST /admin/game HTTP/1.1" 302 Found
    2023-11-15 01:24:30 prijatelitree-web-1       | INFO:     192.168.65.1:59557 - "GET /admin/game HTTP/1.1" 200 OK
    2023-11-15 01:25:05 prijatelitree-web-1       | INFO:     192.168.65.1:59558 - "POST /admin/game HTTP/1.1" 302 Found
    2023-11-15 01:25:05 prijatelitree-web-1       | INFO:     192.168.65.1:59558 - "GET /admin/game HTTP/1.1" 200 OK
    2023-11-15 01:25:06 prijatelitree-web-1       | INFO:     192.168.65.1:59558 - "POST /admin/game HTTP/1.1" 302 Found
    2023-11-15 01:25:06 prijatelitree-web-1       | INFO:     192.168.65.1:59558 - "GET /admin/game HTTP/1.1" 200 OK
    2023-11-15 01:25:08 prijatelitree-web-1       | INFO:     192.168.65.1:59558 - "GET /admin/dashboard HTTP/1.1" 200 OK
    ```

--- prijateli_tree/app/database.py
+++ prijateli_tree/app/database.py
@@ -212,7 +212,7 @@ class Player(Base):
         nullable=False,
     )
     position = Column(Integer, nullable=False)
-    name_hidden = Column(Boolean, nullable=False)
+    name_hidden = Column(Boolean, nullable=False, default=False)
     ready = Column(Boolean, nullable=False, default=False)
     game = relationship(
         "Game",
--- prijateli_tree/app/routers/games.py
+++ prijateli_tree/app/routers/games.py
@@ -8,6 +8,7 @@ from pathlib import Path
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
+from starlette.datastructures import URL
 
 from prijateli_tree.app.database import (
     Game,
@@ -41,6 +42,7 @@ def get_db():
 
 base_dir = Path(__file__).resolve().parent
 templates = Jinja2Templates(directory=str(Path(base_dir, "../templates")))
+templates.env.globals["URL"] = URL
 
 languages = {}
 for lang in glob.glob("../languages/*.json"):

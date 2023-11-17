cf9fb6ac7a4151cf740cc1d7ae1862e4ef2bd20a
Federico Domínguez Molina <fd.molina@outlook.com>
2023-11-17 15:43:11 -0600

    Adding function to get player

--- prijateli_tree/app/routers/test_games.py
+++ prijateli_tree/app/routers/test_games.py
@@ -12,3 +12,28 @@ def test_add_answer(game_id, player_id, answer):
     """
     Adding answer for a given player.
     """
+
+    response = requests.post(
+        f"{API}games/{game_id}/players/{player_id}/answer",
+        json={"answer": answer},
+    )
+    print(response.status_code)
+    print(response.json())
+
+
+def test_get_player(game_id, player_id):
+    """
+    Getting player info.
+    """
+
+    response = requests.get(f"{API}games/{game_id}/players/{player_id}")
+    print(response.status_code)
+    print(response.json())
+
+
+# MAIN
+
+if __name__ == "__main__":
+    # Some tests
+    test_get_player(1, 2)
+    test_add_answer(1, 2, "B")

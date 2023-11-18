e8c65985e66c5ebccb21274d5aa0a3daea619c07
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-11-18 15:46:38 -0600

    Update 2023-11-18-2145_0735fdd31631_.py

--- prijateli_tree/migrations/versions/2023-11-18-2145_0735fdd31631_.py
+++ prijateli_tree/migrations/versions/2023-11-18-2145_0735fdd31631_.py
@@ -5,20 +5,23 @@ Revises: 30991d313ac8
 Create Date: 2023-11-18 21:45:17.443658
 
 """
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 
 
-
-revision = '0735fdd31631'
-down_revision = '30991d313ac8'
+revision = "0735fdd31631"
+down_revision = "30991d313ac8"
 
 
 def upgrade():
-    op.add_column('games', sa.Column('next_game_id', sa.Integer(), nullable=True))
-    op.create_foreign_key('games_next_game_id_fkey', 'games', 'games', ['next_game_id'], ['id'])
+    op.add_column(
+        "games", sa.Column("next_game_id", sa.Integer(), nullable=True)
+    )
+    op.create_foreign_key(
+        "games_next_game_id_fkey", "games", "games", ["next_game_id"], ["id"]
+    )
 
 
 def downgrade():
-    op.drop_constraint('games_next_game_id_fkey', 'games', type_='foreignkey')
-    op.drop_column('games', 'next_game_id')
+    op.drop_constraint("games_next_game_id_fkey", "games", type_="foreignkey")
+    op.drop_column("games", "next_game_id")

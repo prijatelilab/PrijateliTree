ae1fb7fe08ede7a5b0a5720439f464dd537a752a
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-11-18 18:52:28 -0600

    Update games_test.py

--- prijateli_tree/tests/games_test.py
+++ prijateli_tree/tests/games_test.py
@@ -3,13 +3,11 @@ import random
 
 import requests
 
+from prijateli_tree.app.utils.constants import BALL_BLUE, BALL_RED
 
-# Parameters
 
 API = "http://localhost:8000/"
-POSSIBLE_ANSWERS = ["R", "B"]
-
-# Testing functions
+POSSIBLE_ANSWERS = [BALL_RED, BALL_BLUE]
 
 
 def test_add_answer(game_id, player_id, round, answer):

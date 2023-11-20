a861ffe3abb3871b0e71eb6165502e16364149ca
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-11-20 14:34:39 -0600

    Create 2023-11-20-2030_8d6d2f92cb84_.py

---
+++ prijateli_tree/migrations/versions/2023-11-20-2030_8d6d2f92cb84_.py
@@ -0,0 +1,43 @@
+"""empty message
+
+Revision ID: 8d6d2f92cb84
+Revises: 0735fdd31631
+Create Date: 2023-11-20 20:30:45.989585
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+
+revision = '8d6d2f92cb84'
+down_revision = '0735fdd31631'
+
+
+def upgrade():
+    op.drop_column('game_players', 'name_hidden')
+    op.add_column('game_types', sa.Column('names_hidden', sa.Boolean(), server_default='false', nullable=False))
+    op.execute(
+        """
+            INSERT INTO game_types
+            (network, bag, names_hidden)
+            VALUES
+            ('integrated', 'RRRRBB', TRUE),
+            ('segregated', 'RRRRBB', TRUE),
+            ('self-selected', 'RRRRBB', TRUE),
+            ('integrated', 'BBBBRR', TRUE),
+            ('segregated', 'BBBBRR', TRUE),
+            ('self-selected', 'BBBBRR', TRUE);
+        """
+    )
+
+
+def downgrade():
+    op.drop_column('game_types', 'names_hidden')
+    op.add_column('game_players', sa.Column('name_hidden', sa.BOOLEAN(), autoincrement=False, nullable=False))
+    op.execute(
+        """
+            DELETE FROM game_types
+            WHERE names_hidden = TRUE;
+        """
+    )

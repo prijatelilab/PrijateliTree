06f80c3b7299af95479b786c933ff455b201b53f
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-11-20 14:34:48 -0600

    Update 2023-11-20-2030_8d6d2f92cb84_.py

--- prijateli_tree/migrations/versions/2023-11-20-2030_8d6d2f92cb84_.py
+++ prijateli_tree/migrations/versions/2023-11-20-2030_8d6d2f92cb84_.py
@@ -5,18 +5,22 @@ Revises: 0735fdd31631
 Create Date: 2023-11-20 20:30:45.989585
 
 """
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 
 
-
-revision = '8d6d2f92cb84'
-down_revision = '0735fdd31631'
+revision = "8d6d2f92cb84"
+down_revision = "0735fdd31631"
 
 
 def upgrade():
-    op.drop_column('game_players', 'name_hidden')
-    op.add_column('game_types', sa.Column('names_hidden', sa.Boolean(), server_default='false', nullable=False))
+    op.drop_column("game_players", "name_hidden")
+    op.add_column(
+        "game_types",
+        sa.Column(
+            "names_hidden", sa.Boolean(), server_default="false", nullable=False
+        ),
+    )
     op.execute(
         """
             INSERT INTO game_types
@@ -33,8 +37,13 @@ def upgrade():
 
 
 def downgrade():
-    op.drop_column('game_types', 'names_hidden')
-    op.add_column('game_players', sa.Column('name_hidden', sa.BOOLEAN(), autoincrement=False, nullable=False))
+    op.drop_column("game_types", "names_hidden")
+    op.add_column(
+        "game_players",
+        sa.Column(
+            "name_hidden", sa.BOOLEAN(), autoincrement=False, nullable=False
+        ),
+    )
     op.execute(
         """
             DELETE FROM game_types

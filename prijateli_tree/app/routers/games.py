545bef75cc3a5ae11c78f96849039e082e213952
Ari Anisfeld <ari.anisfeld@gmail.com>
2023-11-22 13:55:28 -0500

    Add wait screen logic (#57)
    ## Describe Your Changes
    Add waiting screen to ensure all players have submitted a guess before
    moving on.
    
    ## Checklist Before Requesting a Review
    - [x] The code runs successfully.
    
    ```commandline
    2023-11-21 16:09:35 INFO:     172.20.0.1:54546 - "GET /games/4/player/11/waiting HTTP/1.1" 200 OK
    2023-11-21 16:09:35 INFO:     172.20.0.1:54546 - "GET /games/4/player/static/images/logo.png HTTP/1.1" 404 Not Found
    2023-11-21 16:09:35 INFO:     172.20.0.1:54554 - "GET /css/custom.css HTTP/1.1" 304 Not Modified
    2023-11-21 16:09:35 INFO:     172.20.0.1:54554 - "GET /games/4/player/static/images/logo.png HTTP/1.1" 404 Not Found
    2023-11-21 16:09:40 INFO:     172.20.0.1:35802 - "GET /games/4/all_set HTTP/1.1" 200 OK
    2023-11-21 16:09:46 INFO:     172.20.0.1:35818 - "GET /games/4/all_set HTTP/1.1" 200 OK
    2023-11-21 16:09:51 INFO:     172.20.0.1:39146 - "GET /games/4/all_set HTTP/1.1" 200 OK
    ```
    
    ---------
    
    Co-authored-by: michplunkett <5885605+michplunkett@users.noreply.github.com>

--- prijateli_tree/app/routers/games.py
+++ prijateli_tree/app/routers/games.py
@@ -92,6 +92,20 @@ def get_game_and_player(
     return game, filtered_player[0]
 
 
+def get_lang_from_player_id(player_id: int, db: Depends(get_db)):
+    """
+    Get language form player_id
+    """
+    player = db.query(Player).filter_by(id=player_id).one_or_none()
+
+    if player is None:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail="player not found"
+        )
+
+    return player.language.abbr
+
+
 def did_player_win(
     game: Game,
     player_id: int,
@@ -272,6 +286,47 @@ def view_round(game_id: int, player_id: int, db: Session = Depends(get_db)):
         # Show the player their previous answer and their neighbors
         previous_answers = get_previous_answers(game.id, player.id, db)
         return {"round": current_round, "previous_answers": previous_answers}
+
+
+@router.get("/{game_id}/all_set")
+def all_set(
+    request: Request,
+    game_id: int,
+    db: Session = Depends(get_db),
+):
+    """
+    Determines if all players have submitted a guess in the current round
+    """
+    players = db.query(Player).filter_by(game_id=game_id).all()
+    n_answers = 0
+    for player in players:
+        n_answers += len(player.answers)
+
+    ready = n_answers % len(players) == 0
+
+    return {"ready": ready}
+
+
+@router.get("/{game_id}/player/{player_id}/waiting")
+def waiting(
+    request: Request,
+    game_id: int,
+    player_id: int,
+    db: Session = Depends(get_db),
+):
+    """
+    Wait screen shows until all players are ready to move to the next section
+    """
+    template_text = languages[get_lang_from_player_id(player_id, db)]
+
+    result = {
+        "request": request,
+        "game_id": game_id,
+        "player_id": player_id,
+        "text": template_text,
+    }
+
+    return templates.TemplateResponse("waiting.html", result)
 
 
 @router.post("/{game_id}/player/{player_id}/update_score")

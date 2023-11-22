f0d78a22fa661d95c54ccd18b6d6833b7e33bd60
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-11-22 17:51:28 -0600

    Create 2023-11-22-2351_3d92f1e628d4_.py

---
+++ prijateli_tree/migrations/versions/2023-11-22-2351_3d92f1e628d4_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 3d92f1e628d4
+Revises: 8d6d2f92cb84
+Create Date: 2023-11-22 23:51:01.079824
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+
+revision = '3d92f1e628d4'
+down_revision = '8d6d2f92cb84'
+
+
+def upgrade():
+    op.create_unique_constraint('game_player_id_round_key', 'game_answers', ['game_player_id', 'round'])
+
+
+def downgrade():
+    op.drop_constraint('game_player_id_round_key', 'game_answers', type_='unique')

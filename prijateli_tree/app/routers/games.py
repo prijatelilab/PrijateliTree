7d45cb7cc5502f1ab07ee993fd7fe2692dfdb4ab
Federico Domínguez Molina <fd.molina@outlook.com>
2023-12-14 14:57:42 -0600

    Adding current round to some endpoints

--- prijateli_tree/app/routers/games.py
+++ prijateli_tree/app/routers/games.py
@@ -116,9 +116,7 @@ def view_round(
         )
         return RedirectResponse(url=redirect_url, status_code=HTTPStatus.FOUND)
     else:
-        template_data["previous_answers"] = get_previous_answers(
-            game_id, player_id, db
-        )
+        template_data["previous_answers"] = get_previous_answers(game_id, player_id, db)
 
     return templates.TemplateResponse(
         "round.html", {"request": request, **template_data}
@@ -161,9 +159,7 @@ def route_add_answer(
         db.commit()
         db.refresh(new_answer)
 
-    redirect_url = request.url_for(
-        "waiting", game_id=game_id, player_id=player_id
-    )
+    redirect_url = request.url_for("waiting", game_id=game_id, player_id=player_id)
 
     return RedirectResponse(url=redirect_url, status_code=HTTPStatus.SEE_OTHER)
 
@@ -201,6 +197,7 @@ def waiting(
     _, player = get_game_and_player(game_id, player_id, db)
     player_name, player_score = get_score_and_name(player, db)
     template_text = languages[get_lang_from_player_id(player_id, db)]
+    current_round = get_current_round(game_id, db)
 
     result = {
         "request": request,
@@ -209,6 +206,7 @@ def waiting(
         "text": template_text,
         "player_name": player_name,
         "player_score": player_score,
+        "current_round": current_round,
     }
 
     return templates.TemplateResponse("waiting.html", result)
@@ -258,9 +256,7 @@ def route_get_score(
     )
 
     session_player = (
-        db.query(GameSessionPlayer)
-        .filter_by(id=session_player_id)
-        .one_or_none()
+        db.query(GameSessionPlayer).filter_by(id=session_player_id).one_or_none()
     )
     if session_player is None:
         raise HTTPException(
@@ -409,9 +405,7 @@ def route_session_access(
 
     raise_exception_if_none(session, "session not found")
 
-    return templates.TemplateResponse(
-        "new_session.html", context={"request": request}
-    )
+    return templates.TemplateResponse("new_session.html", context={"request": request})
 
 
 @router.get("/{game_id}")

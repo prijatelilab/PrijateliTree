b24d2f6c325ff44505522b8022b8f6c916cdb291
Federico Domínguez Molina <fd.molina@outlook.com>
2023-12-21 19:24:12 -0600

    Processing sections

--- prijateli_tree/app/languages/process_translations.py
+++ prijateli_tree/app/languages/process_translations.py
@@ -5,7 +5,7 @@
 import pandas as pd
 
 
-FILE_PATH = "app/languages/language_6person.xlsx"
+FILE_PATH = "C:/Users/fdmol/Desktop/Prijateli-project/language_6person.xlsx"
 
 
 class Translator:
@@ -18,7 +18,27 @@ class Translator:
         # Drop rows with NaN values
         self.df.dropna(inplace=True, how="all")
 
-    def get_languages(self):
+    def get_languages_and_sections(self):
         """Gets the languages from the file."""
         columns = self.df.columns
         self.languages = [column for column in columns if len(column) == 2]
+        self.game_sections = self.df.loc[:, "game_section"].unique()
+
+    def generate_dict(self):
+        """Generates a dictionary with the translations."""
+        self.parse_file()
+        self.get_languages()
+
+        # Create a dictionary for each language
+        self.translations = {}
+        for language in self.languages:
+            self.translations[language] = {}
+
+        # Get the translations
+
+
+if __name__ == "__main__":
+    translator = Translator(FILE_PATH)
+    translator.generate_dict()
+
+    print(translator.translations)

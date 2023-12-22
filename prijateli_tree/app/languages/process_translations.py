6db40bf176d40a5f179e27a80eefa016399b1a0b
Federico Domínguez Molina <fd.molina@outlook.com>
2023-12-21 19:36:44 -0600

    Adding preliminary pipeline

--- prijateli_tree/app/languages/process_translations.py
+++ prijateli_tree/app/languages/process_translations.py
@@ -14,12 +14,12 @@ class Translator:
         self.df = pd.read_excel(file_path, sheet_name="Sheet1", header=0)
 
     def parse_file(self):
-        """Parses the file and creates a dictionary with the translations."""
+        """Parses the file"""
         # Drop rows with NaN values
         self.df.dropna(inplace=True, how="all")
 
     def get_languages_and_sections(self):
-        """Gets the languages from the file."""
+        """Gets the languages and game sections from the file."""
         columns = self.df.columns
         self.languages = [column for column in columns if len(column) == 2]
         self.game_sections = self.df.loc[:, "game_section"].unique()
@@ -27,14 +27,32 @@ class Translator:
     def generate_dict(self):
         """Generates a dictionary with the translations."""
         self.parse_file()
-        self.get_languages()
+        self.get_languages_and_sections()
 
         # Create a dictionary for each language
         self.translations = {}
         for language in self.languages:
             self.translations[language] = {}
 
-        # Get the translations
+            # Iterate over the game sections
+            for game_section in self.game_sections:
+                # Get the rows for the current game section
+                game_section_df = self.df.loc[
+                    self.df["game_section"] == game_section
+                ]
+                # Add empty dictionary for the current game section
+                self.translations[language][game_section] = {}
+
+                # Get the translations for the current game section
+                for index, row in game_section_df.iterrows():
+                    # Get subcolumn and translation
+                    subcolumn = row["subcolumn"]
+                    translation = row[language]
+
+                    # Add translation to the dictionary
+                    self.translations[language][game_section][
+                        subcolumn
+                    ] = translation
 
 
 if __name__ == "__main__":

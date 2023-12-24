c521405edfc1a67849fcc4316631903a9e6e069e
michplunkett <5885605+michplunkett@users.noreply.github.com>
2023-12-24 00:20:17 -0600

    Create conftest.py

---
+++ prijateli_tree/tests/conftest.py
@@ -0,0 +1,36 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlmodel import Session, SQLModel, create_engine
+from sqlmodel.pool import StaticPool
+
+from prijateli_tree.app.main import app
+
+
+@pytest.fixture(name="session")
+def session_fixture():
+    engine = create_engine(
+        "sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool
+    )
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        add_mockdata(session)
+        yield session
+
+
+def add_mockdata(session: Session):
+    # Add data you want to the test DB using the `session` object.
+    pass
+
+
+@pytest.fixture(name="client")
+def client_fixture(session: Session):
+    def get_session_override():
+        return session
+
+    app.dependency_overrides['get_session'] = get_session_override
+
+    client = TestClient(app)
+    yield client
+    app.dependency_overrides.clear()
+
+

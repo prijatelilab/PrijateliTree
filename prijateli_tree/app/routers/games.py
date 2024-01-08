376cf812f17220e1b39793ff7d504432ccda2656
Federico Domínguez Molina <fd.molina@outlook.com>
2024-01-07 21:22:33 -0600

    Linting

--- prijateli_tree/app/routers/games.py
+++ prijateli_tree/app/routers/games.py
@@ -108,7 +108,9 @@ def start_session(
     return templates.TemplateResponse("ready.html", result)
 
 
-@router.get("/{game_id}/player/{player_id}/start_of_game", response_class=HTMLResponse)
+@router.get(
+    "/{game_id}/player/{player_id}/start_of_game", response_class=HTMLResponse
+)
 def start_of_game(
     request: Request,
     game_id: int,
@@ -168,7 +170,9 @@ def view_round(
         )
         return RedirectResponse(url=redirect_url, status_code=HTTPStatus.FOUND)
     else:
-        template_data["previous_answers"] = get_previous_answers(game_id, player_id, db)
+        template_data["previous_answers"] = get_previous_answers(
+            game_id, player_id, db
+        )
 
     return templates.TemplateResponse(
         "round.html", {"request": request, **template_data}
@@ -211,12 +215,16 @@ def route_add_answer(
         db.commit()
         db.refresh(new_answer)
 
-    redirect_url = request.url_for("waiting", game_id=game_id, player_id=player_id)
+    redirect_url = request.url_for(
+        "waiting", game_id=game_id, player_id=player_id
+    )
 
     return RedirectResponse(url=redirect_url, status_code=HTTPStatus.SEE_OTHER)
 
 
-@router.get("/{game_id}/player/{player_id}/all_set", response_class=JSONResponse)
+@router.get(
+    "/{game_id}/player/{player_id}/all_set", response_class=JSONResponse
+)
 def all_set(
     game_id: int,
     player_id: int,
@@ -244,7 +252,9 @@ def all_set(
     return JSONResponse(content={"ready": ready, "game_over": game_over})
 
 
-@router.get("/{game_id}/player/{player_id}/waiting", response_class=HTMLResponse)
+@router.get(
+    "/{game_id}/player/{player_id}/waiting", response_class=HTMLResponse
+)
 def waiting(
     request: Request,
     game_id: int,
@@ -273,7 +283,9 @@ def waiting(
     return templates.TemplateResponse("waiting.html", result)
 
 
-@router.put("/{game_id}/player/{player_id}/update_score", response_class=JSONResponse)
+@router.put(
+    "/{game_id}/player/{player_id}/update_score", response_class=JSONResponse
+)
 def update_score(
     game_id: int,
     player_id: int,
@@ -289,7 +301,9 @@ def update_score(
             session_player = get_session_player_from_player(player, db)
             game_status = did_player_win(game, player_id, db)
             session_player.correct_answers += game_status["is_correct"]
-            session_player.points += game_status["is_correct"] * game.winning_score
+            session_player.points += (
+                game_status["is_correct"] * game.winning_score
+            )
         db.commit()
         db.refresh(player)
 
@@ -342,7 +356,9 @@ def route_get_score(
     )
 
     session_player = (
-        db.query(GameSessionPlayer).filter_by(id=session_player_id).one_or_none()
+        db.query(GameSessionPlayer)
+        .filter_by(id=session_player_id)
+        .one_or_none()
     )
     if session_player is None:
         raise HTTPException(
@@ -352,7 +368,9 @@ def route_get_score(
     return JSONResponse(content={"points": session_player.points})
 
 
-@router.get("/{game_id}/player/{player_id}/end_of_game", response_class=HTMLResponse)
+@router.get(
+    "/{game_id}/player/{player_id}/end_of_game", response_class=HTMLResponse
+)
 def end_of_game(
     request: Request,
     game_id: int,
@@ -410,7 +428,9 @@ def go_to_next_game(
         redirect_url = request.url_for(
             "get_qualtrics", game_id=game_id, player_id=player_id
         )
-        return RedirectResponse(url=redirect_url, status_code=HTTPStatus.SEE_OTHER)
+        return RedirectResponse(
+            url=redirect_url, status_code=HTTPStatus.SEE_OTHER
+        )
 
     next_player_id = (
         db.query(GamePlayer)
@@ -548,7 +568,9 @@ def thank_you(
 
 
 @router.get("/{game_id}", response_class=JSONResponse)
-def route_game_access(game_id: int, db: Session = Depends(Database)) -> JSONResponse:
+def route_game_access(
+    game_id: int, db: Session = Depends(Database)
+) -> JSONResponse:
     game = db.query(Game).filter_by(id=game_id).one_or_none()
     raise_exception_if_none(game, detail="game not found")
     return JSONResponse(

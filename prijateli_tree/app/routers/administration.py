8cfe2ce3df75c7a7169c5d20d98383ae559b1dfa
Ari Anisfeld <ari.anisfeld@gmail.com>
2024-01-10 12:11:56 -0600

    #76 making default number of games

--- prijateli_tree/app/routers/administration.py
+++ prijateli_tree/app/routers/administration.py
@@ -40,6 +40,7 @@ from prijateli_tree.app.utils.constants import (
     ROLE_ADMIN,
     ROLE_STUDENT,
     ROLE_SUPER_ADMIN,
+    NUMBER_OF_GAMES,
 )
 from prijateli_tree.app.utils.games import raise_exception_if_not
 
@@ -192,7 +193,10 @@ def create_session(
 ) -> RedirectResponse:
     if user is None:
         return RedirectResponse("login", status_code=HTTPStatus.FOUND)
-
+    
+    if num_games is None:
+        num_games = NUMBER_OF_GAMES
+        logging.info(f"Setting number of games to {num_games}")
     player_ids = [
         player_one,
         player_two,
@@ -232,7 +236,7 @@ def create_session(
             redirect_url,
             status_code=HTTPStatus.FOUND,
         )
-
+    logging.info("Setting up session")
     session = GameSession(
         created_by=user.id,
         num_games=num_games,

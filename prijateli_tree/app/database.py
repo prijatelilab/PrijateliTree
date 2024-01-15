70519480f5e42d5a6ed08d124059ff7a7e562d81
Federico Domínguez Molina <fd.molina@outlook.com>
2024-01-14 20:49:05 -0600

    Linting

--- prijateli_tree/app/database.py
+++ prijateli_tree/app/database.py
@@ -142,7 +142,9 @@ class Denars(Base):
     )
     created_by_session_id = Column(
         Integer,
-        ForeignKey("game_sessions.id", name="denars_created_by_session_id_fkey"),
+        ForeignKey(
+            "game_sessions.id", name="denars_created_by_session_id_fkey"
+        ),
         nullable=True,
     )
     created_by_user_id = Column(
@@ -292,7 +294,9 @@ class GameAnswer(Base):
         back_populates="answers",
     )
     __table_args__ = (
-        UniqueConstraint("game_player_id", "round", name="game_player_id_round_key"),
+        UniqueConstraint(
+            "game_player_id", "round", name="game_player_id_round_key"
+        ),
     )
 
 
@@ -357,7 +361,9 @@ class GameSession(Base):
     )
     # 16 was used as the default, but it can really be any number.
     num_games = Column(Integer, nullable=False, server_default=text("16"))
-    finished = Column(Boolean, nullable=False, server_default=expression.false())
+    finished = Column(
+        Boolean, nullable=False, server_default=expression.false()
+    )
 
     games = relationship(
         "Game",
@@ -422,4 +428,6 @@ class PlayerNetwork(Base):
     )
 
     player = relationship("GamePlayer", foreign_keys="PlayerNetwork.player_id")
-    neighbor = relationship("GamePlayer", foreign_keys="PlayerNetwork.neighbor_id")
+    neighbor = relationship(
+        "GamePlayer", foreign_keys="PlayerNetwork.neighbor_id"
+    )

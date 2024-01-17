0101ec8dde5a39e4e4e3d3cbc126c7c9b8caf2d8
Federico Domínguez Molina <fd.molina@outlook.com>
2024-01-17 00:47:14 -0600

    URL typo fix

--- prijateli_tree/app/routers/games.py
+++ prijateli_tree/app/routers/games.py
@@ -110,9 +110,7 @@ def start_session(
     return templates.TemplateResponse("ready.html", result)
 
 
-@router.get(
-    "/{game_id}/player/{player_id}/start_of_game", response_class=HTMLResponse
-)
+@router.get("/{game_id}/player/{player_id}/start_of_game", response_class=HTMLResponse)
 def start_of_game(
     request: Request,
     game_id: int,
@@ -172,16 +170,14 @@ def view_round(
         )
         return RedirectResponse(url=redirect_url, status_code=HTTPStatus.FOUND)
     else:
-        template_data["previous_answers"] = get_previous_answers(
-            game_id, player_id, db
-        )
+        template_data["previous_answers"] = get_previous_answers(game_id, player_id, db)
 
     return templates.TemplateResponse(
         "round.html", {"request": request, **template_data}
     )
 
 
-@router.get("/{game_id}/player/{player_id}/choose_neigbors")
+@router.get("/{game_id}/player/{player_id}/choose_neighbors")
 def choose_neighbors(
     request: Request,
     game_id: int,
@@ -192,7 +188,6 @@ def choose_neighbors(
     Function that will allow each player to choose their neighbors
     """
     game, player = get_game_and_player(game_id, player_id, db)
-    header = get_header_data(player, db)
 
     template_text = languages[player.language.abbr]
     current_round = get_current_round(game_id, db)
@@ -203,15 +198,12 @@ def choose_neighbors(
     num_neighbors = len(game_util.neighbors[player.position])
 
     template_data = {
-        "practice_game": game.practice,
         "first_round": current_round == 1,
         "text": template_text,
         "player_id": player_id,
         "game_id": game_id,
         "num_neighbors": num_neighbors,
-        "completed_game": player.completed_game,
         "round_progress": f"{current_round}/{game.rounds}",
-        **header,
     }
 
     return templates.TemplateResponse(
@@ -255,16 +247,12 @@ def route_add_answer(
         db.commit()
         db.refresh(new_answer)
 
-    redirect_url = request.url_for(
-        "waiting", game_id=game_id, player_id=player_id
-    )
+    redirect_url = request.url_for("waiting", game_id=game_id, player_id=player_id)
 
     return RedirectResponse(url=redirect_url, status_code=HTTPStatus.SEE_OTHER)
 
 
-@router.get(
-    "/{game_id}/player/{player_id}/all_set", response_class=JSONResponse
-)
+@router.get("/{game_id}/player/{player_id}/all_set", response_class=JSONResponse)
 def all_set(
     game_id: int,
     player_id: int,
@@ -292,9 +280,7 @@ def all_set(
     return JSONResponse(content={"ready": ready, "game_over": game_over})
 
 
-@router.get(
-    "/{game_id}/player/{player_id}/waiting", response_class=HTMLResponse
-)
+@router.get("/{game_id}/player/{player_id}/waiting", response_class=HTMLResponse)
 def waiting(
     request: Request,
     game_id: int,
@@ -323,9 +309,7 @@ def waiting(
     return templates.TemplateResponse("waiting.html", result)
 
 
-@router.put(
-    "/{game_id}/player/{player_id}/update_score", response_class=JSONResponse
-)
+@router.put("/{game_id}/player/{player_id}/update_score", response_class=JSONResponse)
 def update_score(
     game_id: int,
     player_id: int,
@@ -341,9 +325,7 @@ def update_score(
             session_player = get_session_player_from_player(player, db)
             game_status = did_player_win(game, player_id, db)
             session_player.correct_answers += game_status["is_correct"]
-            session_player.points += (
-                game_status["is_correct"] * game.winning_score
-            )
+            session_player.points += game_status["is_correct"] * game.winning_score
         db.commit()
         db.refresh(player)
 
@@ -396,9 +378,7 @@ def route_get_score(
     )
 
     session_player = (
-        db.query(GameSessionPlayer)
-        .filter_by(id=session_player_id)
-        .one_or_none()
+        db.query(GameSessionPlayer).filter_by(id=session_player_id).one_or_none()
     )
     if session_player is None:
         raise HTTPException(
@@ -408,9 +388,7 @@ def route_get_score(
     return JSONResponse(content={"points": session_player.points})
 
 
-@router.get(
-    "/{game_id}/player/{player_id}/end_of_game", response_class=HTMLResponse
-)
+@router.get("/{game_id}/player/{player_id}/end_of_game", response_class=HTMLResponse)
 def end_of_game(
     request: Request,
     game_id: int,
@@ -467,9 +445,7 @@ def go_to_next_game(
         redirect_url = request.url_for(
             "get_qualtrics", game_id=game_id, player_id=player_id
         )
-        return RedirectResponse(
-            url=redirect_url, status_code=HTTPStatus.SEE_OTHER
-        )
+        return RedirectResponse(url=redirect_url, status_code=HTTPStatus.SEE_OTHER)
 
     next_player_id = (
         db.query(GamePlayer)
@@ -607,9 +583,7 @@ def thank_you(
 
 
 @router.get("/{game_id}", response_class=JSONResponse)
-def route_game_access(
-    game_id: int, db: Session = Depends(Database)
-) -> JSONResponse:
+def route_game_access(game_id: int, db: Session = Depends(Database)) -> JSONResponse:
     game = db.query(Game).filter_by(id=game_id).one_or_none()
     raise_exception_if_none(game, detail="game not found")
     return JSONResponse(

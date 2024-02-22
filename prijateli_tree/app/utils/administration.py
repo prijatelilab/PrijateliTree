23bb490b9a29aeb7e0410b98558031b964d62dff
Ari Anisfeld <ari.anisfeld@gmail.com>
2024-02-21 23:17:17 -0600

    bcrypt for pwd + lint (#130)

--- prijateli_tree/app/utils/administration.py
+++ prijateli_tree/app/utils/administration.py
@@ -2,9 +2,25 @@
 
 import random
 
+from passlib.context import CryptContext
+
 from prijateli_tree.app.utils.constants import SHOW_NETWORK_PROBABILITY
 
 
 def show_network():
     """Return True if the network should be shown."""
     return random.random() < SHOW_NETWORK_PROBABILITY
+
+
+# hat tip: https://www.fastapitutorial.com/blog/password-hashing-fastapi/
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+class Hasher:
+    @staticmethod
+    def verify_password(plain_password, hashed_password):
+        return pwd_context.verify(plain_password, hashed_password)
+
+    @staticmethod
+    def get_password_hash(password):
+        return pwd_context.hash(password)

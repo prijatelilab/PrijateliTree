48785f4b97a91e8ed8b863f1b02141107dc6d9d1
Ari Anisfeld <ari.anisfeld@gmail.com>
2024-03-11 18:14:41 -0400

    lint

--- prijateli_tree/app/routers/games.py
+++ prijateli_tree/app/routers/games.py
@@ -9,8 +9,8 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, Form, HTTPException, Request, Response
 from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
-from sqlalchemy.orm import Session
 from sqlalchemy import desc
+from sqlalchemy.orm import Session
 
 from prijateli_tree.app.database import (
     Game,
@@ -82,8 +82,8 @@ def choose_session_players(
 ) -> Response:
     session = (
         db.query(GameSession)
-        .filter_by(session_key=session_key.lower()) 
-        .order_by(desc('created_at'))
+        .filter_by(session_key=session_key.lower())
+        .order_by(desc("created_at"))
         .first()
     )
     if session is None:
